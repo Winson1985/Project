e0c87ca9edd89f254e9fa2d52980312441603fdb
Winson.QIU <qiu.yongx@outlook.com>
2017-11-27 18:56:44 +0800

    String member function practice

--- PracticesCpp/PracticesCpp/PracticesCpp.cpp
+++ PracticesCpp/PracticesCpp/PracticesCpp.cpp
@@ -1,10 +1,21 @@
 // PracticesCpp.cpp : Defines the entry point for the console application.
 //
 
+#include <string>
+
+using namespace std;
 
 
-int main()
-{
-    return 0;
+int main() {
+  string s = "ghyuoko_";
+  if (s.find("_") != string::npos) {
+    size_t i = s.find("_");
+    string s1 = s.substr(0, i);
+    string s2 = s.substr(i + 1, s.size() - i - 1);
+    string s_new = s2 + "_" + s1;
+  }
+  string t = "";
+  bool b = t.empty();
+  return 0;
 }
 
